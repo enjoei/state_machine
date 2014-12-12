bf71c50fb332bf0a10f13c97e42fcd6fee6562fe
Andrey Krivko <jastkand@gmail.com>
2014-12-12 18:54:48 +0600

    move around_validation and around_save methods to public to make gem work with rails 4.1 without patch from #295

--- lib/state_machine.rb
+++ lib/state_machine.rb
@@ -1,6 +1,6 @@
 # By default, requiring "state_machine" means that both the core implementation
 # *and* extensions to the Ruby core (Class in particular) will be pulled in.
-# 
+#
 # If you want to skip the Ruby core extensions, simply require "state_machine/core"
 # and extend StateMachine::MacroMethods in your class.  See the README for more
 # information.
